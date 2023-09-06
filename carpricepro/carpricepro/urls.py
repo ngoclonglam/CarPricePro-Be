3721ba580497de02400ceeb87680aee78b419733
LamLong <50525244+ngoclonglam@users.noreply.github.com>
2023-09-06 22:38:08 +0700

    Update urls.py

--- carpricepro/carpricepro/urls.py
+++ carpricepro/carpricepro/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from api.views import RegisterView, LoginView, ChangePasswordView
+from carpricepro.api.views import RegisterView, LoginView, ChangePasswordView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
