3f441fb0fe777cbbe0019dae115ef6b5a5e8e2fe
LamLong <50525244+ngoclonglam@users.noreply.github.com>
2023-09-08 01:06:45 +0700

    Fix locale problem

--- carpricepro/api/views.py
+++ carpricepro/api/views.py
@@ -154,11 +154,6 @@ class PredictPriceView(APIView):
             print('after exec')
             # Trích xuất tham số từ request.query_params
             print('input_sup')
-            print('This result: ', int(request.query_params.get('chieu_dai_co_so', 0)))
-            print('This result: ', int(request.query_params.get('so_cho_ngoi', 0)))
-            print('This result: ', int(request.query_params.get('chieu_dai_xe', 0)))
-            print('This result: ', int(request.query_params.get('chieu_cao_xe', 0)))
-            print('check it out')
             input_data = {
                 'Chiều Dài Cơ Sở': int(request.query_params.get('chieu_dai_co_so', 0)),
                 'Số Chỗ Ngồi': int(request.query_params.get('so_cho_ngoi', 0)),
